17619727103393c45a96b397f4d5c7e9071db1bb
andrelinces <andrelinces@yahoo.com.br>
2024-10-30 15:47:34 -0300

    feat Bind function changed to (.sink)
    Variables added to the afterEach {} of the test class to free up unused memory.

--- integrating Bitrise/ViewController.swift
+++ integrating Bitrise/ViewController.swift
@@ -10,17 +10,12 @@ import SnapKit
 import Combine
 
 class ViewController: UIViewController {
-    private var viewModel: ViewModel
     
-//    private var numberModel: NumberDataProvider
-//    private var stringModel: StringDataProvider
+    private var viewModel: ViewModel
+    private var cancellables = Set<AnyCancellable>()
     
     @Published var displayedNumbers: [Int] = [] // New published property
     @Published var displayedStrings: [String] = []
-    private var cancellables = Set<AnyCancellable>()
-
-    
-    //testing....
     
     init(viewModel: ViewModel) {
             self.viewModel = viewModel
@@ -28,67 +23,33 @@ class ViewController: UIViewController {
             bindViewModel()
         }
     
-    // Required initializer for Storyboards
+    // Required initializer for Storyboardsor maintain support.
         required init?(coder: NSCoder) {
             let defaultNumberProvider = DefaultNumberDataProvider()
             let defaultStringProvider = DefaultStringDataProvider()
             self.viewModel = ViewModel(numberProvider: defaultNumberProvider, stringProvider: defaultStringProvider)
             super.init(coder: coder)
         }
-    
-    
-    
-    //testing......
-    
-//    init(viewModel: ViewModel = ViewModel(dataProvider: DefaultNumberDataProvider())) {
-//        self.viewModel = viewModel
-//        super.init(nibName: nil, bundle: nil)
-//    }
-
-//    required init?(coder: NSCoder) {
-//        let defaultDataProvider = DefaultNumberDataProvider()
-//        self.viewModel = ViewModel(dataProvider: defaultDataProvider)
-//        super.init(coder: coder)
-//    }
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         bindViewModel()
-//        viewModel.$filteredNumbers
-//            .sink { [weak self] numbers in
-//                self?.displayedNumbers = numbers
-//                print("Numbers greater than 15: \(numbers)")
-//            }
-//            .store(in: &cancellables)
-//        viewModel.$filteredStrings
-//                    .sink { [weak self] string in
-//                        self?.displayedStrings = string
-//                        print("String equal C: \(string)")
-//                    }.store(in: &cancellables)
+
     }
-    
+   
     private func bindViewModel() {
-            // Observing filteredNumbers and filteredStrings from viewModel
-            viewModel.$filteredNumbers
-                .assign(to: &$displayedNumbers)
-        print("Numbers: - \(displayedNumbers)")
-            viewModel.$filteredStrings
-                .assign(to: &$displayedStrings)
-        print("Strings: - \(displayedStrings)")
-        }
-    
-    
-//    private func bindViewModel() {
-//        
-//        viewModel.$filteredNumbers
-//            .sink { [weak self] numbers in
-//                self?.displayedNumbers = numbers
-//                print("Numbers greater than 15: \(numbers)")
-//            }.store(in: &cancellables)
-//        viewModel.$filteredStrings
-//            .sink { [weak self] string in
-//                self?.displayedStrings = string
-//                print("Numbers greater than 15: \(string)")
-//            }.store(in: &cancellables)
-//    }
+        
+        viewModel.$filteredNumbers
+            .sink { [weak self] numbers in
+                self?.displayedNumbers = numbers
+                print("Numbers greater than 15: \(numbers)")
+            }.store(in: &cancellables)
+        viewModel.$filteredStrings
+            .sink { [weak self] string in
+                self?.displayedStrings = string
+                print("Strings equal to C: \(string)")
+            }.store(in: &cancellables)
+    }
 }
--- integrating BitriseTests/integrating_BitriseTests.swift
+++ integrating BitriseTests/integrating_BitriseTests.swift
@@ -19,14 +19,17 @@ class ExampleViewControllerSpec: QuickSpec {
         var viewController: ViewController!
         var cancellables = Set<AnyCancellable>()
 
+        var numberProvider: DefaultNumberDataProvider!
+        var stringProvider: DefaultStringDataProvider!
+        
         beforeEach {
-            // Initialize with the default data providers
+            // Initialize with the default data providers, mock test
 //            let numberProvider = DefaultNumberDataProvider(numbers: [11, 3, 35, 7, 9, 1, 13, 25, 17, 19])
 //            let stringProvider = DefaultStringDataProvider(strings: ["Apple", "Cherry", "Banana", "Citrus"])
             
-            //testing.... values from viewcontroller
-            let numberProvider = DefaultNumberDataProvider()
-            let stringProvider = DefaultStringDataProvider()
+            // test for ViewModel and ViewController
+             numberProvider = DefaultNumberDataProvider()
+             stringProvider = DefaultStringDataProvider()
             
             viewModel = ViewModel(numberProvider: numberProvider, stringProvider: stringProvider)
             viewController = ViewController(viewModel: viewModel)
@@ -37,10 +40,11 @@ class ExampleViewControllerSpec: QuickSpec {
 
         afterEach {
                     cancellables.removeAll()
+            viewModel = nil
+            viewController = nil
+            numberProvider = nil
+            stringProvider = nil
                 }
-        
-        //testing....
-        
         
         describe("ViewController receiving ViewModel values") {
             context("when numbers are provided") {
@@ -109,50 +113,6 @@ class ExampleViewControllerSpec: QuickSpec {
     }
 }
 
-
-
-
-//class ExampleViewControllerSpec: QuickSpec {
-//    override func spec() {
-//        var viewController: ViewController!
-//        var mockDataProvider: MockNumberDataProvider!
-//        
-//        var cancellables = Set<AnyCancellable>()
-//
-//        beforeEach {
-//            // Initialize with the default data provider and viewModel
-//            
-//            mockDataProvider = MockNumberDataProvider(numbers: [10, 20, 30, 55, 3, 77])
-////            let viewModel = ViewModel(dataProvider: mockDataProvider)
-//            
-////            let viewModel = ViewModel(dataProvider: DefaultNumberDataProvider())
-////            viewController = ViewController(viewModel: viewModel)
-//            
-//            // Trigger viewDidLoad to set up viewController
-//            _ = viewController.view
-//        }
-//
-//        describe("viewController with default viewModel data") {
-//            it("displays filtered numbers from the default data provider") {
-//                var receivedNumbers: [Int] = []
-//                
-//                // Subscribe to displayedNumbers in the viewController
-//                viewController.$displayedNumbers
-//                    .sink { numbers in
-//                        receivedNumbers = numbers
-//                    }
-//                    .store(in: &cancellables)
-//                
-//                // Test that displayedNumbers matches the expected filtered numbers
-////                expect(receivedNumbers).toEventually(equal([35, 25, 17, 19]))
-////                expect(receivedNumbers).toEventually(equal([20, 30])) // Mock values
-//                // Check that all received numbers are greater than 15
-//                expect(receivedNumbers).toEventually(allPass { $0 ?? 00 > 15 })
-//                print("receivedNumbers: \(receivedNumbers)")
-//            }
-//        }
-//    }
-//}
 
 
 //MARK: - MOCK NumberDataProvider
